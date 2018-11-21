7a7e5577acac71853b5e00d74c587236ed7f9792
nv-gordeev <nv-gordeev@ya.ru>
2018-11-21 08:24:54 +0500

    add updateElement

--- public/js/my-own-virtual-dom.js
+++ public/js/my-own-virtual-dom.js
@@ -11,6 +11,27 @@ export function createElement(node) {
   }
 
   const $root = document.createElement(node.type);
-  node.children.map(createElement).forEach($root.appendChild.bind($root));
+  node.children && node.children.map(createElement).forEach($root.appendChild.bind($root));
   return $root;
+}
+
+function nodeChanged(node1, node2) {
+  return typeof node1 !== typeof node2 || typeof node1 == 'string' && node1 !== node2 || node1.type !== node2.type;
+}
+
+export function updateElement($parent, newNode, oldNode, index = 0) {
+  if (!oldNode) {
+    $parent.appendChild(createElement(newNode));
+  } else if (!newNode) {
+    $parent.removeChild($parent.childNodes[index]);
+  } else if (nodeChanged(newNode, oldNode)) {
+    $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+  } else if (newNode.type) {
+    const newLength = newNode.children.length;
+    const oldLength = oldNode.children.length;
+
+    for (let i = 0; i < newLength || i < oldLength; i++) {
+      updateElement($parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+    }
+  }
 }
